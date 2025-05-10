640558c6eae6978acd2868daa448f3492aa1d4b7
Mikołaj Wiszniewski <mikowisz@gmail.com>
2025-05-10 20:34:07 +0200

    chore(): formatting

--- a_star.py
+++ a_star.py
@@ -5,10 +5,12 @@ from typing import List, Tuple, Optional
 
 # Manhattan distance
 def h(a: Tuple[int, int], b: Tuple[int, int]) -> int:
-    return abs(a[0]-b[0]) + abs(a[1]-b[1])
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-def aStar(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]) -> [
-    Optional[List[Tuple[int, int]]], float, int]:
+
+def aStar(
+    matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]
+) -> Tuple[Optional[List[Tuple[int, int]]], float, int]:
     startTime = time.time()
     width = len(matrix[0])
     height = len(matrix)
@@ -16,7 +18,9 @@ def aStar(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]
     nodesVisited = 0
     pathTracker = {}
     priorityQueue = []
-    hq.heappush(priorityQueue, (0+h(entry, exit), 0, entry)) # stored like: (fScore, gScore, position)
+    hq.heappush(
+        priorityQueue, (0 + h(entry, exit), 0, entry)
+    )  # stored like: (fScore, gScore, position)
     gScores = {entry: 0}
 
     while priorityQueue:
@@ -27,14 +31,21 @@ def aStar(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]
             while position in pathTracker:
                 position = pathTracker[position]
                 path.insert(0, position)
-            return path, time.time()-startTime, nodesVisited
+            return path, time.time() - startTime, nodesVisited
         for directionX, directionY in directions:
-            x, y = position[0]+directionX, position[1]+directionY
+            x, y = position[0] + directionX, position[1] + directionY
             if matrix[y][x] == 1 or 0 > x >= width or 0 > y >= height:
                 continue
             adjacentPos = (x, y)
-            if adjacentPos not in gScores or gScore+1 < gScores[adjacentPos]:
-                gScores[adjacentPos] = gScore+1
+            if adjacentPos not in gScores or gScore + 1 < gScores[adjacentPos]:
+                gScores[adjacentPos] = gScore + 1
                 pathTracker[adjacentPos] = position
-                hq.heappush(priorityQueue, (gScores[adjacentPos]+h(adjacentPos, exit), gScores[adjacentPos], adjacentPos))
-    return None, time.time()-startTime, nodesVisited
+                hq.heappush(
+                    priorityQueue,
+                    (
+                        gScores[adjacentPos] + h(adjacentPos, exit),
+                        gScores[adjacentPos],
+                        adjacentPos,
+                    ),
+                )
+    return None, time.time() - startTime, nodesVisited
--- dfs.py
+++ dfs.py
@@ -2,7 +2,9 @@ from typing import List, Tuple, Optional
 import time
 
 
-def dfsRecursive(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]) -> [Optional[List[Tuple[int, int]]], float, int]:
+def dfsRecursive(
+    matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]
+) -> Tuple[Optional[List[Tuple[int, int]]], float, int]:
     startTime = time.time()
     width = len(matrix[0])
     height = len(matrix)
@@ -13,7 +15,15 @@ def dfsRecursive(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[in
 
     def dfs(x: int, y: int) -> bool:
         nonlocal nodesVisited
-        if matrix[y][x] == 1 or isVisited[y][x] or x < 0 or x >= width or y < 0 or y >= height or nodesVisited >= 1500:
+        if (
+            matrix[y][x] == 1
+            or isVisited[y][x]
+            or x < 0
+            or x >= width
+            or y < 0
+            or y >= height
+            or nodesVisited >= 1500
+        ):
             return False
         isVisited[y][x] = True
         nodesVisited += 1
@@ -21,17 +31,19 @@ def dfsRecursive(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[in
         if (x, y) == exit:
             return True
         for directionX, directionY in directions:
-            if dfs(x+directionX, y+directionY):
+            if dfs(x + directionX, y + directionY):
                 return True
         path.pop()
         return False
 
     if dfs(entry[0], entry[1]):
-        return path, time.time()-startTime, nodesVisited
-    return None, time.time()-startTime, nodesVisited
+        return path, time.time() - startTime, nodesVisited
+    return None, time.time() - startTime, nodesVisited
 
-def dfsIterative(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]) -> [
-    Optional[List[Tuple[int, int]]], float, int]:
+
+def dfsIterative(
+    matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[int, int]
+) -> Tuple[Optional[List[Tuple[int, int]]], float, int]:
     startTime = time.time()
     width = len(matrix[0])
     height = len(matrix)
@@ -50,6 +62,6 @@ def dfsIterative(matrix: List[List[int]], entry: Tuple[int, int], exit: Tuple[in
         if (x, y) == exit:
             return currentPath, time.time() - startTime, nodesVisited
         for directionX, directionY in directions:
-            stack.append((x+directionX, y+directionY, currentPath))
+            stack.append((x + directionX, y + directionY, currentPath))
 
-    return None, time.time()-startTime, nodesVisited
+    return None, time.time() - startTime, nodesVisited
